8751260319297b6e43988544ab3b8e6687a89259
oriane <bessaoriane53@gmail.com>
2026-09-25 12:01:24 +0200

    quatrieme commit

---
+++ src/main/kotlin/monstres/EspeceMonstres.kt
@@ -0,0 +1,50 @@
+package monstres
+
+/**
+* Représente un entraîneur dans le contexte du jeu.
+*
+* Un entraîneur est responsable de gérer une équipe de monstres, une boîte pour stocker des monstres supplémentaires
+* et un sac contenant des objets appelés MonsterKubes. L'entraîneur a également une somme d'argent associée.
+*
+* @property id L'identifiant unique de l'entraîneur.
+* @property nom Le nom de l'entraîneur.
+* @property argents La quantité d'argent en possession de l'entraîneur.
+
+*/
+
+class EspeceMonstres(
+    var id : Int,
+    var nom: String,
+    var type: String,
+    val baseAttaque: Int,
+    val baseDefense: Int,
+    val baseVitesse: Int,
+    val baseAttaqueSpe: Int,
+    val baseDefenseSpe: Int,
+    val basePv: Int,
+    val modAttaque: Double,
+    val modDefense: Double,
+    val modVitesse: Double,
+    val modAttaqueSpe: Double,
+    val modDefenseSpe: Double,
+    val modPv: Double,
+    val description: String = "",
+    val particularites: String = "",
+    val caractères: String = "",
+){
+    /**
+     * Affiche la représentation artistique ASCII du monstre.
+     *
+     * @param deFace Détermine si l'art affiché est de face (true) ou de dos (false).
+     *               La valeur par défaut est true.
+     * @return Une chaîne de caractères contenant l'art ASCII du monstre avec les codes couleur ANSI.
+     *         L'art est lu à partir d'un fichier texte dans le dossier resources/art.
+     */
+    fun afficheArt(deFace: Boolean=true): String{
+        val nomFichier = if(deFace) "front" else "back";
+        val art=  File("src/main/resources/art/${this.nom.lowercase()}/$nomFichier.txt").readText()
+        val safeArt = art.replace("/", "∕")
+        return safeArt.replace("\\u001B", "\u001B")
+    }
+
+}
